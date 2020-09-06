481ad296e2e3e395e662d44173710e7c997aba17
tapendra-195 <tapendra@neutrino.uwinnipeg.ca>
2020-09-06 05:59:56 -0500

    Final tested working version of program to find if two ellipses intersect

---
+++ ellipse_intersection.cpp
@@ -0,0 +1,423 @@
+/*################################################################################
+#Code to classify if two ellipse intersect, one is inside other, or separated    #
+#based on:https://www.geometrictools.com/Documentation/IntersectionOfEllipses.pdf#
+#                                                                                #  
+##################################################################################*/
+
+#include "ellipse_intersection.hpp"
+#include <iostream>
+const int debug = 0;
+
+using namespace cv;
+
+//based on article :https://www.geometrictools.com/Documentation/IntersectionOfEllipses.pdf
+//Sin(x), cos(x) function only use angle in radian in input.
+double degree_to_Rad(double a){
+  double PI = acos(-1);
+  return (a*PI/180.0);
+}
+
+int EllipseIntersect::intersect(const ellipse_st e0, const ellipse_st e1){
+  //Getting parameters for first ellipse
+  double a0 = e0.get_a();
+  double b0 = e0.get_b();
+  double ee0 = e0.get_e();
+  double phi0 = (ee0==0.0)?0.0:degree_to_Rad(e0.get_phi());
+  xypoint cent0 = e0.get_xy();
+  
+  //Getting parameters for Second ellipse
+  double a1 = e1.get_a();
+  double b1 = e1.get_b();
+  double ee1 = e1.get_e();
+  double phi1 = (ee1==0.0)?0.0:degree_to_Rad(e1.get_phi());
+  xypoint cent1 = e1.get_xy();
+  
+  //Ellipse equation is (X-K)^TM(X-K)=1
+  //X(vector) is variable; K(vector) = centre of ellipse; 
+  //M(matrix) = U0U0/l0^2+U0U1^T/l1^2; where U = unit vector along ellipse axis
+  //M is positive definite matrix=> symmetric matrix with all +ve eigenvalue.
+  Matx21d K0(cent0.x,
+	     cent0.y);
+  
+  //M = RDR^T (eigenvalue decomposition; R is orthogonal so R^-1 = R^T)
+  //R0=matrix with column vector(unit) pointing in direction of ellipse axis.
+  Matx22d R0(cos(phi0), -1.0*sin(phi0),
+	     sin(phi0), cos(phi0));
+ 
+  Matx22d D0(1.0/(a0*a0), 0.,
+	     0., 1.0/(b0*b0));
+    
+    
+  //parameters of ellipse 1
+  Matx21d K1(cent1.x,
+	     cent1.y);
+  
+  Matx22d R1(cos(phi1), -1.0*sin(phi1),
+	     sin(phi1), cos(phi1));
+  
+  Matx22d D1(1.0/(a1*a1), 0,
+	     0, 1.0/(b1*b1));
+
+  //D0^-0.5 
+  Matx22d D0NegHalf(a0, 0,
+		    0, b0);
+    
+  //D0Half = D0^0.5
+  Matx22d D0Half(1.0/a0, 0,
+		 0, 1.0/b0);
+    
+  //K= centre of transformed ellipse(2)(variable changed to Y)  
+  Matx21d K2 = D0Half*(R0.t())*(K1-K0);
+  //Compute M2
+  Matx22d D0NegHalfR0TR1 = D0NegHalf*(R0.t())*R1;
+  
+  //M2 = D^-0.5R0^TR1D1R^TR0D0^-0.5
+  Matx22d M2 = D0NegHalfR0TR1*D1*(D0NegHalfR0TR1.t());
+  
+  //M2 = RDR^T
+  //M2 is symmetric matrix(when the shape is ellipse).
+  std::vector<double> eigenvalues;
+  Matx22d eigenvector;
+  eigen(M2,eigenvalues, eigenvector);
+    
+  //eigen() outputs eigenvector in each row, but we want eigenvectors in column
+  Matx22d R = eigenvector.t();
+  std::vector<double> D = eigenvalues;
+  
+  //compute K=R^T*K2
+  //K is centre of second ellipse after transforming and aligning axis.
+  Matx21d K = R.t()*K2;
+  
+  if(debug){
+
+    std::cout<<"M200 = "<<M2(0,0)<<std::endl;
+    std::cout<<"M201 = "<<M2(0,1)<<std::endl;
+    std::cout<<"M210 = "<<M2(1,0)<<std::endl;
+    std::cout<<"M211 = "<<M2(1,1)<<std::endl;
+    std::cout<<"eigen vector"<<std::endl;
+    std::cout<<R(0,0)<<" "<<R(0,1)<<std::endl;
+    std::cout<<R(1,0)<<" "<<R(1,1)<<std::endl;
+    std::cout<<"M2"<<std::endl;
+    std::cout<<M2(0,0)<<" "<<M2(0,1)<<std::endl;
+    std::cout<<M2(1,0)<<" "<<M2(1,1)<<std::endl;
+
+    //Drawing the transformed and axis aligned version.
+    Mat mpa = cv::Mat(cv::Size(500, 500), CV_64FC3, Scalar(0,0,0));
+    Point center(  250, 250);
+    circle(mpa, center, 30,Scalar (0, 255, 0) , 1);
+    Size axes1( 30.0/std::sqrt(D[0]), 30.0/std::sqrt(D[1] ));
+    Point center1(  30*K(0,0)+250, 30*K(1,0)+250);
+    ellipse( mpa, center1, axes1, 0, 0., 360,  Scalar (0, 0, 255), 1 );
+    imwrite("transformedandaxisaligned.jpg", mpa);
+
+    //Drawing the transformed but axis not aligned version.
+    Mat mpa2 = cv::Mat(cv::Size(500, 500), CV_64FC3, Scalar(0,0,0));
+    Point center0(  250, 250);
+    circle(mpa2, center0, 30,Scalar (0, 255, 0) , 1);
+    Size axes2( 30.0/std::sqrt(D[0]), 30.0/std::sqrt(D[1]) );
+    Point center2(  30*K2(0,0)+250, 30*K2(1,0)+250);
+    ellipse( mpa2, center2, axes2, e1.get_phi(), 0., 360,  Scalar (0, 0, 255), 1 );
+    imwrite("transformedbutaxisnotaligned.jpg", mpa2);
+  }
+    
+ //Transform ellipse0 is Z^T*Z =1 and transformed ellipse 1 is  (Z-K)^T*D*(Z-K)
+    double minSqrDistance = std::numeric_limits<double>::max();
+    double maxSqrDistance = 0.;
+    
+    //The special cacse of common centre
+    if(K==Matx21d(0,0)){
+      for(int i=0; i<2; ++i){
+	double invD = 1.0/D[i];
+	if(invD < minSqrDistance){
+	  minSqrDistance = invD;
+	}
+	if(invD>maxSqrDistance){
+	  maxSqrDistance = invD;
+	}
+      }
+    
+      return Classify(minSqrDistance, maxSqrDistance, 0);
+    }
+    
+    //closest 
+    double d0p = D[0], d1p = D[1];
+    double c0p = K(0,0)*K(0,0), c1p = K(1,0)*K(1,0);
+
+    if(debug){
+    std::cout<<"d0= "<<d0<<std::endl;
+    std::cout<<"d1= "<<d1<<std::endl;
+    }
+    
+    //sort to make d0>= d1.which allows to bound root of f(s)= d0*k0^2/(d0*s-1)^2+ d1*k1^2/(d1*s-1)^2-1
+    std::vector<cv::Point2d>param(2);
+    //if axis(0) bigger than axis 1 for transformed ellipse.
+    //making bigger axis as major axis.
+    if(d0p>=d1){
+      param[0] = cv::Point2d(d0p, c0p); 
+      param[1] = cv::Point2d(d1p, c1p);
+    }
+    else{
+      param[0] = cv::Point2d(d1p, c1p);
+      param[1] = cv::Point2d(d0p, c0p);
+    }
+
+    std::vector<cv::Point2d>valid;
+    //if d0>d1
+    //if major axis bigger than minor.
+    if(param[0].x>param[1].x){
+      for(int i=0; i<2; ++i){
+	//if the coordinate isn't origin.
+	if(param[i].y>0){
+	  valid.push_back(param[i]);
+	}
+      }
+        }
+    //if d0==d1
+    else{
+      if(param[0].y+param[1].y>0){ //we want atleast one of them to be >0 else the case will be case of same centre which is handled previously.
+	valid.push_back(param[0]);
+      }
+    }
+    
+    if(valid.size()==1){	
+      d0=valid[0].x;
+      c0=valid[0].y;
+      d1=param[1].x;
+      c1=param[1].y;
+    }
+    else if(valid.size()==2){
+      d0=valid[0].x;
+      c0=valid[0].y;
+      d1=valid[1].x;
+      c1=valid[1].y;
+    }
+
+    
+    size_t numValid = valid.size();
+    int numRoots = 0;
+    double roots[4];
+
+    if(numValid==2){
+      GetRoots(valid[0].x, valid[1].x, valid[0].y, valid[1].y, numRoots, roots);
+    }
+    else if(numValid ==1){
+      GetRoots(valid[0].x, valid[0].y, numRoots, roots);
+    }
+    
+    //numvalid cannot be zero because we already handled K=0 case.
+    for(int i=0; i<numRoots; ++i){
+      double s = roots[i];
+      double p0 = d0*K(0,0)*s/(d0*s-1.0);
+      double p1 = d1*K(1,0)*s/(d1*s-1.0);
+      double sqrDistance = p0*p0+p1*p1;
+      if(sqrDistance < minSqrDistance){
+	minSqrDistance = sqrDistance;
+      }
+      if(sqrDistance > maxSqrDistance ){
+	maxSqrDistance = sqrDistance;
+      }
+    }
+
+    if(debug){
+    std::cout<<"maxSqrdist = "<<maxSqrDistance<<std::endl;
+    std::cout<<"minSqrdist = "<<minSqrDistance<<std::endl;
+    std::cout<<"d0p*c0p+d1p*c1p = "<<d0p*c0p+d1p*c1p<<std::endl;
+    std::cout<<"d0 = "<<d0<<std::endl;
+    std::cout<<"d1 = "<<d1<<std::endl;
+    std::cout<<"c0 = "<<c0<<std::endl;
+    std::cout<<"c1 = "<<c1<<std::endl;
+    std::cout<<"d0p = "<<d0p<<std::endl;
+    std::cout<<"d1p = "<<d1p<<std::endl;
+    std::cout<<"c0p = "<<c0p<<std::endl;
+    std::cout<<"c1p = "<<c1p<<std::endl;
+    }
+
+    return Classify(minSqrDistance, maxSqrDistance, d0*c0+d1*c1);
+}
+
+int EllipseIntersect::Classify(double minSqrDistance, double maxSqrDistance, double z){
+  if (maxSqrDistance < 1.0)
+    {
+      return ELLIPSE0_STRICTLY_CONTAINS_ELLIPSE1;
+    }
+  else if (maxSqrDistance > 1.0)
+    {
+      if (minSqrDistance < 1.0)
+        {
+	  return ELLIPSES_OVERLAP;
+        }
+      else if (minSqrDistance > 1.0)
+        {//circle centre outside of ellipse.
+	  //z = (h/a)^2+(k/b)^2 for ellipse 1
+	  if (z > 1.0)
+            {
+	      return ELLIPSES_SEPARATED;
+            }
+	  else
+            {
+	      return ELLIPSE1_STRICTLY_CONTAINS_ELLIPSE0;
+            }
+        }
+      else  // minSqrDistance = 1
+        {//circle center outside ellipse
+	  if (z>1.0)//d0c0pd1c1 > 1.0)
+            {
+	      return ELLIPSE0_OUTSIDE_ELLIPSE1_BUT_TANGENT;
+            }
+	  else
+            {
+	      return ELLIPSE1_CONTAINS_ELLIPSE0_BUT_TANGENT;
+            }
+        }
+    }
+  else  // maxSqrDistance = 1
+    {
+      if (minSqrDistance < 1.0)
+        {
+	  return ELLIPSE0_CONTAINS_ELLIPSE1_BUT_TANGENT;
+        }
+      else // minSqrDistance = 1
+        {
+	  return ELLIPSES_EQUAL;
+        }
+    }
+}
+
+//function f(s)
+double EllipseIntersect::F(double s){
+  double invN0 =1.0/(d0*s-1.0);
+  double invN1 = 1.0/(d1*s-1.0);
+  double term0 = d0*c0*invN0*invN0;
+  double term1 = d1*c1*invN1*invN1;
+  double f = term0+term1-1.0;
+  return f;
+}
+
+/*
+//function f'(s)
+double EllipseIntersect::DF(double s){
+  double invN0 = 1.0/(d0*s-1.0);
+  double invN1 = 1.0/(d1*s-1.0);
+  double term0 = d0*d0*c0*invN0*invN0*invN0;
+  double term1 = d1*d1*c1*invN1*invN1*invN1;
+  double df = -2.0*(term0+term1);
+  return df;
+}
+*/
+double EllipseIntersect::BisectionRoot(double smin, double smax,double f0, double f1, unsigned int maxIterations, double &root){
+  
+  root = smin;
+  if(smin<smax){
+    
+    if(f0==0.){
+      root = smin;
+      return 1;
+    }
+   
+    if(f1==0.){
+      root = smax;
+      return 1;
+    }
+    if(f0*f1>0.){
+      return 0;
+    }
+    unsigned int i;
+    for(i=2; i<maxIterations; ++i){
+      root = 0.5*(smin+smax);
+      if(root == smin|| root == smax){
+	break;
+      }
+      double fm= F(root);
+    
+      double product = fm*f0;
+      if(product<0.){
+	smax=root;
+	f1 = fm;
+      }
+      else if (product >0.){
+	smin=root;
+	f0=fm;
+      }
+      else{
+	break;
+      }
+    }
+    return i;
+  }
+  else{
+    return 0;
+  }
+}
+
+//d0>d1
+void EllipseIntersect::GetRoots(const double &x1, const double &x2, const double &y1, const double &y2, int &numRoots, double *roots){
+  
+  const unsigned int maxIterations = (unsigned int)(3+std::numeric_limits<double>::digits - std::numeric_limits<double>::min_exponent);
+  
+  //finding smallest root.
+  unsigned int iterations;
+  numRoots = 0;
+  double smin, smax, s, fval;
+  
+  if(debug){
+  std::cout<<"d0 = "<<d0<<std::endl;
+  std::cout<<"d1 = "<<d1<<std::endl;
+  }
+  
+  //f(0)
+  //if f(0)<=0 then root bounding interval is [0,1/d0]
+  //else it is [(1-(1+f(0))^0.5)/d1,0]
+  double f0 = d0*c0+d1*c1-1.0;//F(0);
+   smax = 1.0/d0;
+
+  if(f0>0.0){
+    smin = ((1.0-std::sqrt(d0*c0+d1*c1))/d1);//f(smin) should be <0 because the function is increasing in the interval.
+    // smax = 0.0;
+    fval = F(smin);
+  }
+  else if(f0<0.0){
+    smin = 0.0;
+   
+  }
+
+  if(f0!=0){
+    iterations = BisectionRoot(smin, smax,-1,1, maxIterations, s);
+    fval = F(s);
+    roots[numRoots++]=s;
+  }
+  else{
+    roots[numRoots++]=0.0;
+  }
+
+  double rho = std::pow((d0*d0*c0/(d1*d1*c1)),(1.0/3.0));
+  double smid = (1.0+rho)/(d0+rho*d1);
+  double fmid = F(smid);
+  if(fmid <0.0){
+    smin = 1.0/d0;
+    smax = smid;
+    iterations = BisectionRoot(smin, smax,1,-1, maxIterations, s);
+    roots[numRoots++] = s;
+
+    smin = smid;
+    smax = 1.0/d1;
+    iterations = BisectionRoot(smin, smax,-1,1, maxIterations, s);
+    roots[numRoots++] = s;
+  }
+  else if(fmid==0.0){
+    roots[numRoots++]=smid;
+  }
+
+  //last root
+  smin = 1.0/d1;
+  smax = (1.0+std::sqrt(d0*c0+d1*c1))/d1;
+  iterations = BisectionRoot(smin, smax,1,-1, maxIterations, s);
+  roots[numRoots++]=s;
+}
+
+//case when the second transformed ellipse is also circle.
+void EllipseIntersect::GetRoots(const double &x1, const double &y1, int &numRoots, double *roots){
+  //here c0 = k0^2; c1 = k1^2. This has been done before sending here so just use c0.
+  roots[0] = (1.0-std::sqrt(d0*(c0+c1)))/d0;
+  roots[1] = (1.0+std::sqrt(d0*(c0+c1)))/d0;
+  numRoots =2; 
+}
---
+++ test_ellipse_intersect.cpp
@@ -0,0 +1,140 @@
+#include "ellipse_intersection.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+const int debug =0;
+using namespace cv; 
+
+//Function used to test performance of ellipse_intersection.cpp class
+//The program seperates the cases of operlapping ellipse, separated ellipse etc. in different folders [0 1 2 3 4 5 6 7]
+//Instructions:
+//mkdir runs
+//cd run
+//mkdir 0 1 2 3 4 5 6 7 txt
+//get out of run and make executable.
+//make test_ellipse_intersect
+//to make file needs ellipse.cpp, ellipse_intersection.cpp as library
+//after the executable is made run ./test_ellipse_intersect
+//  
+
+int main(){
+  /* Testing how eigen() function works in openCv.
+  Matx22d M2(2.0, 3.0,
+	     4.0, 1.0);
+  std::cout<<"sin(30) = "<<sin(30*3.14/180)<<std::endl;
+  std::cout<<M2(0,0)<<M2(0,1)<<std::endl;
+  std::cout<<M2(1,0)<<M2(1,1)<<std::endl;
+  std::vector<double> eigenvalues;
+  Matx22d eigenvectors;
+  eigenNonSymmetric(M2,eigenvalues, eigenvectors);
+
+  //long double a[2][2]={2,0,0,2};
+  //long double b[2][2]={3,0,0,3};
+  //long double c[2][2]=a*b;
+  
+  std::cout<<eigenvalues[0]<<" "<<eigenvalues[1]<<" size "<<eigenvalues.size()<<std::endl;
+  std::cout<<"===================="<<std::endl;
+  std::cout<<eigenvectors(0,0)<<eigenvectors(0,1)<<std::endl;
+  std::cout<<eigenvectors(1,0)<<eigenvectors(1,1)<<std::endl;
+ */
+  srand (time(NULL));
+  
+  for(int i=0; i<1; i++){
+  Mat m = cv::Mat(cv::Size(500, 500), CV_64FC3, Scalar(0,0,0));
+  
+  
+  double phi0 =(rand() %180);
+  double b0=(rand()%60+10);
+  double e0=(rand()%10)/10.0;
+  double a0=b0/std::sqrt(1-e0*e0);
+  
+  double x0=(rand()%150+90);
+  double y0=(rand()%150+90);
+  Size axes(  a0, b0 );
+  Point center( x0, y0 );
+
+  ellipse( m, center, axes, phi0, 0., 360,  Scalar (0, 255, 0), 1 );
+
+  double phi1 =(rand() %180);
+  double b1=(rand()%60+10);
+  double e1=(rand()%10)/10.0;
+  double a1=b1/std::sqrt(1-e1*e1);
+  double x1=(rand()%150+90);
+  double y1=(rand()%150+90);
+  
+  std::string file_name = "./runs/txt/ellipse_test"+std::to_string(i)+".txt";
+  std::ofstream outfile;
+  outfile.open(file_name);
+  
+
+  if(debug){
+    std::cout<<"x0 = "<<x0<<std::endl;
+    std::cout<<"y0 = "<<y0<<std::endl;
+    std::cout<<"a0 = "<<a0<<std::endl;
+    std::cout<<"b0 = "<<b0<<std::endl;
+    std::cout<<"e0 = "<<e0<<std::endl;
+    std::cout<<"phi0 = "<<phi0<<std::endl;
+
+    std::cout<<"e1 = "<<e1<<std::endl;
+    std::cout<<"a1 = "<<a1<<std::endl;
+    std::cout<<"b1 = "<<b1<<std::endl;
+    std::cout<<"phi1 = "<<phi1<<std::endl;
+    std::cout<<"x1 = "<<x1<<std::endl;
+    std::cout<<"y1 = "<<y1<<std::endl;
+  
+    outfile<<"x0 = "<<x0<<std::endl;
+    outfile<<"y0 = "<<y0<<std::endl;
+    outfile<<"a0 = "<<a0<<std::endl;
+    outfile<<"b0 = "<<b0<<std::endl;
+    outfile<<"e0 = "<<e0<<std::endl;
+    outfile<<"phi0 = "<<phi0<<std::endl;
+
+    outfile<<"e1 = "<<e1<<std::endl;
+    outfile<<"a1 = "<<a1<<std::endl;
+    outfile<<"b1 = "<<b1<<std::endl;
+    outfile<<"phi1 = "<<phi1<<std::endl;
+    outfile<<"x1 = "<<x1<<std::endl;
+    outfile<<"y1 = "<<y1<<std::endl;
+   
+  }
+  
+  Size axes1(  a1, b1 );
+  Point center1( x1, y1 );
+  
+  ellipse( m, center1, axes1, phi1, 0., 360,  Scalar (0, 0, 255), 1 );
+
+  ellipse_st first(b0, e0, phi0, xypoint(x0,y0));
+  ellipse_st second(b1, e1, phi1, xypoint(x1,y1));
+  
+  
+  EllipseIntersect E;
+  int inters = E.intersect(first, second);
+  
+  std::string text;
+  switch(inters){
+  case 0: text = "Ellipse Separated"; break;
+  case 1: text="Ellipses Overlap"; break;
+  case 2: text="Ellipse0(Green) outside Ellipse1(Red) But Tangent"; break;
+  case 3: text="Ellipse0(Green) strictly contains Ellipse1(Red)"; break;
+  case 4: text="Ellipse0(Green) strictly contains Ellipse1(Red) but tangent"; break;
+  case 5: text="Ellipse1(Red) strictly contains Ellipse0(Green)"; break;
+  case 6: text="Ellipse1(Red) contains Ellipse0(Green) but tangent"; break;
+  case 7: text="Ellipses Equal"; break;
+  }
+  
+
+  putText(m, "Green= Ellipse 0; Red = Ellipse 1", cv::Point(2,400),FONT_HERSHEY_SIMPLEX , 0.5, cv::Scalar(0,0,255) );
+  
+  putText(m, text, cv::Point(2,480),FONT_HERSHEY_SIMPLEX , 0.5, cv::Scalar(0,0,255) );
+
+  putText(m, std::to_string(inters), cv::Point(2,500),FONT_HERSHEY_SIMPLEX , 0.5, cv::Scalar(0,0,255) );
+  
+  // std::string name = "./runs/"+std::to_string(inters)+"/ellipse_test"+std::to_string(i)+".jpg";
+  //  imwrite(name, m);
+   imwrite("ellipse.jpg", m);
+
+  outfile.close();
+   }
+  return 0;
+}
